3bce519cf480751a472c6fa41db9cc1dbbbdf5e6
Anthony Vuong <anthonyvuong28@yahoo.com>
2021-10-14 15:23:19 -0700

    Time: 64 ms (9.20%), Space: 14.5 MB (18.55%) - LeetHub

---
+++ maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        counter = {"b":0, "a":0, "l":0, "o":0, "n":0}
+        for char in text:
+            if char in counter:
+                counter[char] += 1
+        counter["l"] //= 2
+        counter["o"] //= 2
+        return min(counter.values())
