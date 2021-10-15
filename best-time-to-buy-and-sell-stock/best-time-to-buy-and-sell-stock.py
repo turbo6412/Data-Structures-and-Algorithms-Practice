5bdfb604087ee7870ae937b42e4510fa4d97699e
Anthony Vuong <anthonyvuong28@yahoo.com>
2021-10-14 18:43:43 -0700

    Time: 1347 ms (26.14%), Space: 25.2 MB (11.46%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,21 @@
 class Solution:
-#     prices = [7,1,5,3,6,4]
-# min = 7
-# profit1 -= arr[i] -min
-# profit = max(profit, profit1)
+    """
+    Input: prices = [7,1,5,3,6,4]
+    Output: 5
+    
+             i 
+    [7,1,5,3,6,4]
+    5-1, 3-1, 6-1, 4-1
+    
+    # find smallest entry point to buy stock
+    """
     def maxProfit(self, arr: List[int]) -> int:
-        c_min = float('inf') 
-        profit = float('-inf') 
+        profit = 0
+        start_price = float('inf')
         
-        for i in range(len(arr)): 
-            c_min = min(arr[i], c_min)
-            current_profit = arr[i] - c_min 
-            profit = max(current_profit, profit)
+        for i in range(len(arr)):
+            start_price = min(start_price, arr[i])
+            current_prof = arr[i] - start_price
+            profit = max(profit, current_prof)
         
         return profit 
